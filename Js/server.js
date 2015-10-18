eae3e08ede6ddd3cf6db49066b43b35864381412
KO527 <Kwajo.oseiakoto@gmail.com>
2015-10-18 17:04:39 -0400

    updated server.js

--- Js/server.js
+++ Js/server.js
@@ -4,20 +4,34 @@ var app = express();
 
 
 
-app.use('/', express.static('/Users/KO527/Sites/TarasDeli'));
-app.use('/TarasPhotos', express.static('/Users/KO527/Sites/TarasDeli/TarasPhotos'));
-app.use(express.static('/Users/KO527/Sites/TarasDeli/index'));
 
+//  app.use('/', express.static('/Users/KO527/Sites/TarasDeli'));
+//  app.use('/TarasPhotos', express.static('/Users/KO527/Sites/TarasDeli/TarasPhotos'));
+//  app.use(express.static('/Users/KO527/Sites/TarasDeli/index'));
+
+//  app.get('/', function(req, res){
+//      res.sendFile(path.join(__dirname, './index', '/index.html'));
+//   });
+
+// app.use(express.static('/Users/KO527/Sites/TarasDeli/Contact'));
+
+// app.get('/Contact', function(req, res){
+//     res.sendFile(path.join(__dirname, './Contact', '/Contact.html'));
+// });
+
+app.use('/', express.static('/public_html'));
+
+app.use('/TarasPhotos', express.static('/public_html/TarasPhotos'));
+
+app.use(express.static('/public_html/index'));
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, './index', '/index.html'));
- });
-
-app.use(express.static('/Users/KO527/Sites/TarasDeli/Contact'));
+ 	res.sendFile(path.join(__dirname, './index', '/index.html'));
+});
+app.use(express.static('/public_html/Contact'));
 
 app.get('/Contact', function(req, res){
-    res.sendFile(path.join(__dirname, './Contact', '/Contact.html'));
+ 	res.sendFile(path.join(__dirname, './Contact', '/Contact.html'));
 });
-
 
 app.listen(8080);
 
